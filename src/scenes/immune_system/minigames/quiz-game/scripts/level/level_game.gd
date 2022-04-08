c69d72772ceb906c99d2917d212579386e9d4a34
Joseph Avila <josdavilalvarez@gmail.com>
2022-04-08 01:38:34 -0500

    add question quiz game

--- src/scenes/immune_system/minigames/quiz-game/scripts/level/level_game.gd
+++ src/scenes/immune_system/minigames/quiz-game/scripts/level/level_game.gd
@@ -6,40 +6,65 @@ enum QuestionType { TEXT, IMAGE, VIDEO, AUDIO }
 export(Resource) var bd_quiz
 export(Color) var color_right
 export(Color) var color_wrong
-
 var buttons := []
 var index := 0
 var quiz_shuffle := []
 var correct := 0
 var timer := 10
+var data := []
+var information := [
+{  
+	"question": "Se considera como uno de los autores de la teoría celular",
+	"options": ["Janssen" ,"Hooke","Leewenhoek","Virchow"],
+	"correct": "Hooke"
+ },{  
+	"question": "Un organismo procariótico se caracteriza por carecer de:",
+	"options": ["Plasmalema" ,"Citoplasma","Carioteca","Ribosomas"],
+	"correct": "Carioteca"
+ },{  
+	"question": "La circulación intracelular se realiza a través de:",
+	"options": ["El retículo endoplasmático" ,"Los ribosomas","Los centrosomas","Los dictiosomas"],
+	"correct": "El retículo endoplasmático"
+ },
+{  
+	"question": "La digestión tanto intracelular como extracelular es realizada por:",
+	"options": ["R. endoplasmático  rugoso" ,"R. endoplasmático liso","Peroxisoma","Lisosomas"],
+	"correct": "Lisosomas"
+ }
+]
+
+
+
+
+
 
 onready var question_texts := $question_info/txt_question
+
+func load_save():
+	return information
 
 func _ready() -> void:
 	for _button in $question_holder.get_children():
 		buttons.append(_button)
 	for _button in $question_holder2.get_children():
 		buttons.append(_button)
-	
-	quiz_shuffle = randomize_array(bd_quiz.bd)
+	quiz_shuffle = load_save()
+	data = load_save()
 	load_quiz()		
 
-		
 func load_quiz() -> void:
-	if index >= bd_quiz.bd.size():
+	if index >= data.size():
 		game_over()
 		return
-	
-	question_texts.text = str(quiz_shuffle[index].question_info)
-	
-	var options = randomize_array(bd_quiz.bd[index].options)	
+	question_texts.text = str(quiz_shuffle[index].question)
+	var options = randomize_array(data[index].options)	
 	for i in buttons.size():
 		buttons[i].text = str(options[i])
 		buttons[i].connect("pressed", self, "buttons_answer", [buttons[i]])
 		
 
 func buttons_answer(button) -> void:
-	if bd_quiz.bd[index].correct == button.text:
+	if data[index].correct == button.text:
 		button.modulate = color_right
 		correct += 1
 		$audio_correct.play()
@@ -50,10 +75,7 @@ func buttons_answer(button) -> void:
 	_next_question()
 		
 		
-func _next_question() -> void:
-	
-	timer = 10
-	
+func _next_question() -> void:	
 	yield(get_tree().create_timer(1), "timeout")
 	for bt in buttons:
 		bt.modulate = Color.white
@@ -72,7 +94,7 @@ func randomize_array(array: Array) -> Array:
 
 func game_over() -> void:
 	$game_over.show()
-	$game_over/txt_result.text = str(correct, "/", bd_quiz.bd.size())
+	$game_over/txt_result.text = str(correct, "/", data.size())
 
 func _on_button_restart_pressed():
 	get_tree().reload_current_scene()
